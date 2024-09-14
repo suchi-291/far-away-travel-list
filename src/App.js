acd374c5dd46c7bc823d052b2d860ce42806787f
Suchitha Ramachandrapurapu <66826980+suchi-291@users.noreply.github.com>
2024-09-14 13:04:38 +0530

    created states, handleSubmit funtion, value, set value for form fields

--- src/App.js
+++ src/App.js
@@ -13,18 +13,27 @@ function Logo() {
     </>
   );
 }
+
 function Form() {
   const [description, setDescription] = useState("");
-
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
